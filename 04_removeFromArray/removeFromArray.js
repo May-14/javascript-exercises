e12a8a4e590716d7cea530f9b23f28c3e4968fbb
May <26matmoran@gmail.com>
2024-03-13 14:32:46 -0600

    Finish exercise number 4 of odin's js challenges

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToBeRemoved) {
+    array = array.filter(notEqualTo);
+    function notEqualTo(elementToCheck) {
+        for (element of elementsToBeRemoved) {
+            if (elementToCheck === element) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
