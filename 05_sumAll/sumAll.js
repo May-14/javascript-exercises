f03a0234645c1a1b6a1b0bb41e31f91c410804ea
May <26matmoran@gmail.com>
2024-03-13 14:47:59 -0600

    Finish exercise 5 of odin's js challenges

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let totalSum = 0
+    let numberToAdd = 1;
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0 ) {
+        return "ERROR"
+    }
+    if (num1 > num2) {
+        numberToAdd = -1;
+    }
+    do {
+        totalSum += num1;
 
+        num1 += numberToAdd;
+    } while (num1 -numberToAdd  !== num2);
+    return totalSum
 };
 
 // Do not edit below this line
