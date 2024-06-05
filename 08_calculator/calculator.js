d5d75c6aff52585f5f040e273f82a6581751da36
May <26matmoran@gmail.com>
2024-06-05 14:53:58 -0600

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let power = num1;
+	for (let i = 0; i < num2 - 1; i++) {
+    power = power * num1;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 1) {
+    num = 1
+  }
+  let factorial = num;
+	for (let i = num - 1; i !== 0; i = i -1) {
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
