cc8c5796170422415760933069c19b41523cb30b
May <26matmoran@gmail.com>
2024-06-07 18:20:54 -0600

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    arrayFromString = string.split("");
+    let reverseArray = [];
+    let filteredArrayFromString = [];
+    arrayFromString.forEach((character, index) => {
+        if (character === " " || character === "!" || character === "." || character === "," || character === "?") {
+        } else {
+            filteredArrayFromString.push(character.toLowerCase());
+            reverseArray.unshift(character.toLowerCase());
+        }
+    })
+    let stringFromArray = filteredArrayFromString.join("");
+    let reverseStringFromArray = reverseArray.join("");
+    if (stringFromArray === reverseStringFromArray) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS"
+    }
+    let fibonacciSequence = [0];
+    let lastNumber = 0;
+    let secondToLastNumber = 1;
+    for (let i = 0; i < number + 1; i++) {
+        let newNumber = secondToLastNumber + lastNumber;
+        fibonacciSequence.push(newNumber);
+        secondToLastNumber = lastNumber;
+        lastNumber = newNumber;
+    }
+    return fibonacciSequence[number]
 };
 
 // Do not edit below this line
