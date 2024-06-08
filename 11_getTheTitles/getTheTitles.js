b2c23a0f6c748d3912d2a461618ff0dce4664ae6
May <26matmoran@gmail.com>
2024-06-07 18:24:55 -0600

    Finish exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let arrayOfTitles = array.map(book => book.title);
+    return arrayOfTitles;
 };
 
 // Do not edit below this line
