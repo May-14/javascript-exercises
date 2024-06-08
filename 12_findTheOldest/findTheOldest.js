3aa11bb4d9dd06fec278f70b730c9cdfec9ec001
May <26matmoran@gmail.com>
2024-06-07 19:03:09 -0600

    Finish last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+   return array.reduce((oldestPerson, newPerson, index) => {
+    if (oldestPerson.yearOfDeath === undefined) {
+        oldestPerson.yearOfDeath = new Date().getFullYear();
+    } 
+    let oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    let newPersonAge = newPerson.yearOfDeath - newPerson.yearOfBirth;
+    return oldestPersonAge > newPersonAge ? oldestPerson : newPerson;
+   })
 };
 
 // Do not edit below this line
